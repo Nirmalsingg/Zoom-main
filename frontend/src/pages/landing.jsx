eb7ba564e05c39d52f5c165ca2474109cc9b0fcb
Nirmal Singh <nirmal@email.com>
2026-03-13 23:21:44 +0530

    UI update

--- frontend/src/pages/landing.jsx
+++ frontend/src/pages/landing.jsx
@@ -10,7 +10,7 @@ export default function LandingPage() {
         <div className='landingPageContainer'>
             <nav>
                 <div className='navHeader'>
-                    <h2>Apna Video Call</h2>
+                    <h2>VANS Video Call</h2>
                 </div>
                 <div className='navlist'>
                     <p onClick={() => {
@@ -32,11 +32,11 @@ export default function LandingPage() {
 
             <div className="landingMainContainer">
                 <div>
-                    <h1><span style={{ color: "#FF9839" }}>Connect</span> with your loved Ones</h1>
+                    <h1><span style={{ color: "#ffffff" }}>Stay Close,</span> No Matter The Distance</h1>
 
-                    <p>Cover a distance by Apna Video Call</p>
+                    <p>Experience smooth video calls with friends, family, and teams anytime, anywhere.</p>
                     <div role='button'>
-                        <Link to={"/auth"}>Get Started</Link>
+                        <Link to={"/auth"}>Start Calling</Link>
                     </div>
                 </div>
                 <div>
